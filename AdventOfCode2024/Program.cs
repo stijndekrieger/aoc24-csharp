16fdaff9e2edaae84a0ae9bee911ed68beba09e6
AUXILIUM\sdekrieger <sdekrieger@auxilium.nl>
2024-12-02 14:42:24 +0100

    Day 2 part 2

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -4,4 +4,5 @@ using AdventOfCode2024.Day02.Parts;
 Console.WriteLine("Advent of Code 2024 by Stijn de Krieger");
 Console.WriteLine("Day 1 Part 1: " + Day01Part1.Run()); // 1197984
 Console.WriteLine("Day 1 Part 2: " + Day01Part2.Run()); // 23387399
-Console.WriteLine("Day 2 Part 1: " + Day02Part1.Run());
+Console.WriteLine("Day 2 Part 1: " + Day02Part1.Run()); // 549
+Console.WriteLine("Day 2 Part 2: " + Day02Part2.Run());
