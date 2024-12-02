165d4e12bdaf53828e3eb2d031fc4a705dc58a8c
stijndekrieger <stijndekrieger@gmail.com>
2024-12-02 20:21:17 +0100

    Day 3 prep

---
+++ AdventOfCode2024/Day03/Parts/Part2.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2024.Day03.Parts;
+
+public static class Day03Part2
+{
+    public static int Run()
+    {
+        //var reports = Utils.GetNumberListFromTextInput(File.ReadAllLines("Day03/Data/Input.txt"));
+
+        return 0;
+    }
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,8 +1,13 @@
 ﻿using AdventOfCode2024.Day01.Parts;
 using AdventOfCode2024.Day02.Parts;
+using AdventOfCode2024.Day03.Parts;
 
 Console.WriteLine("Advent of Code 2024 by Stijn de Krieger");
 Console.WriteLine("Day 1 Part 1: " + Day01Part1.Run()); // 1197984
 Console.WriteLine("Day 1 Part 2: " + Day01Part2.Run()); // 23387399
+Console.WriteLine("========");
 Console.WriteLine("Day 2 Part 1: " + Day02Part1.Run()); // 549
 Console.WriteLine("Day 2 Part 2: " + Day02Part2.Run()); // 589
+Console.WriteLine("========");
+Console.WriteLine("Day 3 Part 1: " + Day03Part1.Run()); // ?
+Console.WriteLine("Day 3 Part 2: " + Day03Part2.Run()); // ?
