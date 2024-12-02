c6945e56dac26094c78ad7930cd625fa3f6b42aa
AUXILIUM\sdekrieger <sdekrieger@auxilium.nl>
2024-12-02 17:28:21 +0100

    kevin part 2

--- AdventOfCode2024/Day02/Parts/Part2.cs
+++ AdventOfCode2024/Day02/Parts/Part2.cs
@@ -8,67 +8,75 @@ public static class Day02Part2
     {
         var reports = Utils.GetNumberListFromTextInput(File.ReadAllLines("Day02/Data/Input.txt"));
 
-        int safeReports = 0;
+        var safeReports = 0;
 
         foreach (var report in reports)
         {
-            int? previousNumber = null;
-            bool? descending = null;
-            //bool isReportSafe = false;
-            int failedLevels = 0;
-
-            foreach (var level in report)
-            {
-                if (previousNumber == null) // First iteration
-                {
-                    previousNumber = level;
-                    continue;
-                }
-
-                if (descending == null) // Second iteration
-                {
-                    if (previousNumber > level) descending = true;
-                    else if (previousNumber < level) descending = false;
-                    else
-                    {
-                        failedLevels++;
-                        continue;
-                    }
-
-                    if (!IsValidLevel(previousNumber.Value, level, descending.Value))
-                    {
-                        failedLevels++;
-                        continue;
-                    }
-                    previousNumber = level;
-                    continue;
-                }
-
-                // All other iterations
-                if (!IsValidLevel(previousNumber.Value, level, descending.Value))
-                {
-                    failedLevels++;
-                    continue;
-                }
-                previousNumber = level;
-
-            }
-
-            if (failedLevels <= 1) safeReports++;
+            var isValid = CheckReport(report);
+            if (isValid) safeReports++;
         }
 
         return safeReports;
     }
 
-    private static bool IsValidLevel(int previousNumber, int level, bool descending)
+    private static bool CheckReport(List<int> report, bool allowBadIndices = true)
     {
-        if (previousNumber == level) return false;
+        var isValid = true;
+        //var (_, order) = CalculateDifference(report.First(), report.Skip(1).First()); // TODO: don't lock order based on first result
+        var orders = new Dictionary<int, Order>();
+        var badIndices = new List<int>();
 
-        if (int.Abs(previousNumber - level) > 3) return false;
+        for (var i = 0; i < report.Count - 1; i++)
+        {
+            var isCurrentLevelValid = true;
+            var level = report[i];
+            var nextLevel = report[i + 1];
+            var (difference, deltaOrder) = CalculateDifference(level, nextLevel);
 
-        if (previousNumber > level && !descending) return false;
-        if (previousNumber < level && descending) return false;
+            orders.Add(i, deltaOrder);
 
-        return true;
+            if (difference == 0 || difference > 3) isCurrentLevelValid = false;
+            //if (deltaOrder != order) isCurrentLevelValid = false;
+
+            if (!isCurrentLevelValid) badIndices.Add(i);
+        }
+
+        var groupedOrders = orders.GroupBy(x => x.Value);
+        if (groupedOrders.Any(go => go.Count() == 1))
+        {
+            var badGroupedOrder = groupedOrders.Where(go => go.Count() == 1).First();
+            var badIndex = badGroupedOrder.First().Key;
+            if (!badIndices.Contains(badIndex)) badIndices.Add(badIndex);
+        }
+
+        if (allowBadIndices)
+        {
+            foreach (var badIndex in badIndices)
+            {
+                var alteredReport = report;
+                alteredReport.RemoveAt(badIndex);
+                var succeeded = CheckReport(alteredReport, false);
+                if (succeeded) return true;
+            }
+        }
+
+        if (badIndices.Count > 0) isValid = false;
+
+        return isValid;
     }
+
+    private static (int difference, Order order) CalculateDifference(int previous, int next)
+    {
+        var difference = previous - next;
+        var order = difference == 0 ? Order.Equal : difference > 0 ? Order.Descending : Order.Ascending;
+        return (Math.Abs(difference), order);
+    }
+
+}
+
+public enum Order
+{
+    Equal,
+    Ascending,
+    Descending
 }
