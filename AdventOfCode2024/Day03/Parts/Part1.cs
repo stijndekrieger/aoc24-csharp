b5ba795e92a900663488a28d102a6e102ec3eb80
stijndekrieger <stijndekrieger@gmail.com>
2024-12-03 10:09:41 +0100

    Day 3 part 1

--- AdventOfCode2024/Day03/Parts/Part1.cs
+++ AdventOfCode2024/Day03/Parts/Part1.cs
@@ -1,11 +1,18 @@
-﻿namespace AdventOfCode2024.Day03.Parts;
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day03.Parts;
 
 public static class Day03Part1
 {
     public static int Run()
     {
-        //var reports = Utils.GetNumberListFromTextInput(File.ReadAllLines("Day03/Data/Input.txt"));
+        var input = string.Join("", File.ReadAllLines("Day03/Data/Input.txt"));
 
-        return 0;
+        var regex = new Regex(@"mul\(([0-9]{1,3}),([0-9]{1,3})\)");
+        var matches = regex.Matches(input);
+
+        var total = matches.Sum(m => int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value));
+
+        return total;
     }
 }
