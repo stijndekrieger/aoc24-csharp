efdf97af1bd7d1e9850d862ba920846b3146696d
stijndekrieger <stijndekrieger@gmail.com>
2024-12-05 10:10:52 +0100

    Day 5 part 1

--- AdventOfCode2024/Day05/Parts/Part1.cs
+++ AdventOfCode2024/Day05/Parts/Part1.cs
@@ -1,9 +1,43 @@
-﻿namespace AdventOfCode2024.Day05.Parts;
+﻿using AdventOfCode2024.Utilities;
+
+namespace AdventOfCode2024.Day05.Parts;
 
 public class Day05Part1
 {
     public static int Run()
     {
-        return 0;
+        var input = File.ReadAllLines("Day05/Data/Input.txt");
+        var (pageOrderingRules, pagesToProduce) = Utils.GetTupleListsFromTextInput(input);
+
+        var middleNumberCount = 0;
+
+        foreach (var page in pagesToProduce) if (IsValidPage(page, pageOrderingRules)) middleNumberCount += GetMiddleNumber(page);
+
+        return middleNumberCount;
+    }
+
+    private static bool IsValidPage(List<int> page, List<(int, int)> pageOrderingRules)
+    {
+        var isValid = true;
+
+        var relevantRules = pageOrderingRules
+            .Where(rule => page.Contains(rule.Item1) && page.Contains(rule.Item2))
+            .ToList();
+
+        foreach (var rule in relevantRules)
+        {
+            if (page.IndexOf(rule.Item1) > page.IndexOf(rule.Item2))
+            {
+                isValid = false;
+                break;
+            }
+        }
+
+        return isValid;
+    }
+
+    private static int GetMiddleNumber(List<int> page)
+    {
+        return page[page.Count / 2];
     }
 }
