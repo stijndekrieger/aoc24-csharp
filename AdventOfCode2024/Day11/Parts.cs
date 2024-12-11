9c142fa378074f611eff40f4b399fb2a2ea6b8ea
AUXILIUM\sdekrieger <sdekrieger@auxilium.nl>
2024-12-11 10:39:53 +0100

    Day 11 part 1

--- AdventOfCode2024/Day11/Parts.cs
+++ AdventOfCode2024/Day11/Parts.cs
@@ -2,13 +2,50 @@
 
 public class Day11
 {
-    public static int Part1()
+    private static readonly List<long> Stones = File.ReadAllText("Day11/Data/Input.txt").Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+    private static readonly int MULTIPLY_NUMBER = 2024;
+
+    public static long Part1()
     {
-        return 0;
+        var stones = new List<long>(Stones);
+
+        for (int i = 0; i < 25; i++) stones = Blink(stones);
+
+        return stones.Count;
     }
 
-    public static int Part2()
+    public static long Part2()
     {
-        return 0;
+        var stones = new List<long>(Stones);
+
+        for (int i = 0; i < 75; i++)
+        {
+            stones = Blink(stones);
+            Console.WriteLine($"Currently on blink {i + 1}");
+        }
+
+        return stones.Count;
+    }
+
+    private static List<long> Blink(List<long> stones)
+    {
+        var stonesAfterBlink = new List<long>();
+
+        foreach (var stone in stones)
+        {
+            // Check which operation to do
+            if (stone == 0) stonesAfterBlink.Add(1);
+            else if (stone.ToString().Length % 2 == 0)
+            {
+                var newStone = stone.ToString();
+                int halfLength = newStone.Length / 2;
+
+                stonesAfterBlink.Add(long.Parse(newStone[..halfLength]));
+                stonesAfterBlink.Add(long.Parse(newStone[halfLength..]));
+            }
+            else stonesAfterBlink.Add(stone * MULTIPLY_NUMBER);
+        }
+
+        return stonesAfterBlink;
     }
 }
